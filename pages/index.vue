f6a1b0a03e31c06bd8a6c3b4a4371a284acffba0
Nam <namnt@ints.vn>
2023-05-17 11:28:28 +0700

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const error = ref('')
+
+// import from vue use
+const {
+  isSupported,
+  orientation,
+  lockOrientation,
+  unlockOrientation,
+} = useScreenOrientation()
+
+function full() {
+  // only can switch the screen in full screen mode
+  const element = document.documentElement // Use the root element for fullscreen
+
+  if (element.requestFullscreen)
+    element.requestFullscreen()
+
+  // Lock to landspace
+  lockOrientation('landscape').catch(e => error.value = e)
+}
+</script>
+
+<template>
+  <div>
+    is support: {{ isSupported }} <br>
+    <button mr-10 bg-red-400 px-2 py-1 @click="full">
+      Rotate
+    </button>
+    <!-- Press to unlock -->
+    <button bg-red-400 px-2 py-1 @click="unlockOrientation">
+      Unlock
+    </button>
+    <br>
+    Orentiation: {{ orientation }} <br>
+    Error : {{ error }}
+  </div>
+</template>
